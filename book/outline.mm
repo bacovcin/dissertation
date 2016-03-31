60c7937cc9c8b036f61d86422151910380f1e7dc
bacovcin <bacovcin@babel.ling.upenn.edu>
2016-03-31 11:54:04 -0400

    Rough Draft of Part 2 and First Draft of Chapter 2

---
+++ book/outline.mm
@@ -0,0 +1,136 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1459102450029" ID="ID_305575231" MODIFIED="1459116550839" TEXT="Dissertation">
+<node CREATED="1459116583677" ID="ID_789701426" MODIFIED="1459116587829" POSITION="right" TEXT="Introduction"/>
+<node CREATED="1459116526332" ID="ID_1966687533" MODIFIED="1459116528905" POSITION="right" TEXT="Symmetric and asymmetric ditransitive passives are caused by the interaction of subjecthood locality and dative-to-nominative raising">
+<node CREATED="1459104055340" ID="ID_1570744855" MODIFIED="1459104466666" TEXT="Subject raising and nominative case assignment can show different locality properties">
+<node CREATED="1459104379959" ID="ID_596523990" MODIFIED="1459107787914" TEXT="Base generation and scrambling">
+<node CREATED="1459104731422" ID="ID_729812747" MODIFIED="1459107878889" TEXT="All Germanic languages have recipient--theme order, but Icelandic doesn&apos;t have the theme--recipient order"/>
+<node CREATED="1459116706166" ID="ID_1895478379" MODIFIED="1459116827576" TEXT="Non recipient ditransitives">
+<node CREATED="1459116754073" ID="ID_1270569877" MODIFIED="1459116842702" TEXT="Non dative-accusative ditransitives">
+<node CREATED="1459116789108" ID="ID_216235210" MODIFIED="1459116803956" TEXT="Restricted to Old English, Icelandic (and German?)"/>
+<node CREATED="1459116804470" ID="ID_1873569386" MODIFIED="1459116821606" TEXT="Behave more like prepositional object constructions"/>
+</node>
+<node CREATED="1459116847171" ID="ID_516752109" MODIFIED="1459116850674" TEXT="Benefactives">
+<node CREATED="1459116853996" ID="ID_538320782" MODIFIED="1459116870143" TEXT="All languages have `for&apos; (even though many don&apos;t have recipient `to&apos;)"/>
+<node CREATED="1459116870623" ID="ID_1089398610" MODIFIED="1459116902674" TEXT="Non-`for&apos; benefactives extremely restricted (English and German) and lexical remnants in Dutch"/>
+</node>
+<node CREATED="1459116906039" ID="ID_89826732" MODIFIED="1459116927892" TEXT="Goals and Addressees">
+<node CREATED="1459116917895" ID="ID_982854271" MODIFIED="1459116944599" TEXT="Old English `to&apos; used for goals and addressees"/>
+<node CREATED="1459116945054" ID="ID_1479618106" MODIFIED="1459116952315" TEXT="Two `to&apos;s in Modern English"/>
+</node>
+</node>
+<node CREATED="1459107748400" ID="ID_1043255124" MODIFIED="1459107820551" TEXT="Evidence of scrambling in German">
+<node CREATED="1459107824813" ID="ID_470523792" MODIFIED="1459107829888" TEXT="Focus facts"/>
+<node CREATED="1459107830387" ID="ID_187561314" MODIFIED="1459107843710" TEXT="Evidence of VP-internal orders"/>
+<node CREATED="1459107844368" ID="ID_1171278162" MODIFIED="1459107894347" TEXT="Scope asymmetries"/>
+</node>
+</node>
+<node CREATED="1459104395766" ID="ID_1665033209" MODIFIED="1459107800207" TEXT="Prepositional object constructions and case">
+<node CREATED="1459108249948" ID="ID_1088593736" MODIFIED="1459108255724" TEXT="Morphological analysis">
+<node CREATED="1459108258754" ID="ID_1990280990" MODIFIED="1459108275522" TEXT="Dative case = null ~ dative case = `to&apos;"/>
+<node CREATED="1459108276021" ID="ID_1093447437" MODIFIED="1459108292799" TEXT="Dative case = `to&apos; ~ dative case = null / verb"/>
+<node CREATED="1459108295758" ID="ID_1332109577" MODIFIED="1459108343787" TEXT="Theme pronoun cliticization/recipient pronoun cliticization"/>
+</node>
+<node CREATED="1459107917546" ID="ID_1637556300" MODIFIED="1459107930885" TEXT="Evidence supporting transformation in Dative Shift">
+<node CREATED="1459107957763" ID="ID_1215064539" MODIFIED="1459107973976" TEXT="Resolution of proposed meaning differences (Hovav and Levin)"/>
+<node CREATED="1459107986521" ID="ID_1426351645" MODIFIED="1459107995559" TEXT="Discursus on nominalisation">
+<node CREATED="1459108028675" ID="ID_593561666" MODIFIED="1459108040792" TEXT="Many languages don&apos;t allow datives in nominalisations"/>
+<node CREATED="1459108041295" ID="ID_365258309" MODIFIED="1459108050515" TEXT="Some languages (e.g. Russian) do"/>
+<node CREATED="1459108058321" ID="ID_1693185951" MODIFIED="1459108075934" TEXT="Note that English nominalisations are more verbal than in many languages"/>
+</node>
+<node CREATED="1459107934958" ID="ID_1763328897" MODIFIED="1459107941112" TEXT="Scope asymmetries"/>
+<node CREATED="1459107941533" ID="ID_140321390" MODIFIED="1459107984167" TEXT="Purpose clause facts (Hallman)"/>
+<node CREATED="1459117925466" ID="ID_1036194565" MODIFIED="1459117931099" TEXT="Cross-linguistic evidence">
+<node CREATED="1459117934353" ID="ID_1858694395" MODIFIED="1459117961294" TEXT="Low German (seperation of recipient marking and word order)"/>
+<node CREATED="1459117962261" ID="ID_791085534" MODIFIED="1459117980365" TEXT="Afrikaans (prepositional oblique subjects)"/>
+<node CREATED="1459117984959" ID="ID_1765053036" MODIFIED="1459117994302" TEXT="Dutch (`aan&apos; in both word orders)"/>
+</node>
+</node>
+<node CREATED="1459108083136" ID="ID_1340129942" MODIFIED="1459108093537" TEXT="Historical data">
+<node CREATED="1459108095840" ID="ID_463169673" MODIFIED="1459108106545" TEXT="Old English didn&apos;t have dative shift"/>
+<node CREATED="1459108107049" ID="ID_1835508977" MODIFIED="1459108122525" TEXT="Middle English use of `to&apos; shows Constant Rate Hypothesis in both orders"/>
+<node CREATED="1459108122985" ID="ID_1588844067" MODIFIED="1459108164480" TEXT="Late Middle/Early Modern `to&apos; use">
+<node CREATED="1459108168677" ID="ID_75791046" MODIFIED="1459108181607" TEXT="German does not allow scrambling of PPs (check Old English)"/>
+<node CREATED="1459108182295" ID="ID_1276184227" MODIFIED="1459108217490" TEXT="Late Middle/Early Modern English objects after PPs generally through Heavy NP shift (i.e. same rate as after AdvP)"/>
+<node CREATED="1459108218148" ID="ID_759250657" MODIFIED="1459108239238" TEXT="`to&apos; use in recipient--theme order much higher than Heavy NP shift rate"/>
+</node>
+</node>
+</node>
+<node CREATED="1459104418789" ID="ID_1421008743" MODIFIED="1459104434137" TEXT="Oblique subjects/Nominative objects">
+<node CREATED="1459108348839" ID="ID_739876959" MODIFIED="1459108394565" TEXT="Theme passivisation with bare recipients">
+<node CREATED="1459108373921" ID="ID_824262236" MODIFIED="1459108402269" TEXT="Not derived from active forms">
+<node CREATED="1459108405153" ID="ID_782184627" MODIFIED="1459108412713" TEXT="Actives only allow pronominal themes"/>
+<node CREATED="1459108413078" ID="ID_670813994" MODIFIED="1459108417703" TEXT="American English change"/>
+</node>
+<node CREATED="1459108422855" ID="ID_794220252" MODIFIED="1459108457486" TEXT="Seems to be derived via alternation between direct movement of theme"/>
+</node>
+<node CREATED="1459108360191" ID="ID_1322001489" MODIFIED="1459108467016" TEXT="Iceland evidence for direct movement of theme"/>
+<node CREATED="1459108468588" ID="ID_512556237" MODIFIED="1459108479947" TEXT="German and Dutch evidence for nominative locality"/>
+</node>
+</node>
+<node CREATED="1459104073140" ID="ID_1290839949" MODIFIED="1459107739602" TEXT="Datives can either be: invisible for nominative case, defective interveners for nominative case, or accessible for nominative case">
+<node CREATED="1459104110777" ID="ID_1705134780" MODIFIED="1459104134518" TEXT="Invisible datives">
+<node CREATED="1459104154256" ID="ID_1237312615" MODIFIED="1459104164501" TEXT="Icelandic theme passives"/>
+<node CREATED="1459104164964" ID="ID_262378241" MODIFIED="1459104181706" TEXT="Nominative objects (Icelandic)"/>
+<node CREATED="1459104182217" ID="ID_8281473" MODIFIED="1459104190431" TEXT="In-situ nominatives in German and Dutch"/>
+<node CREATED="1459104190910" ID="ID_296861820" MODIFIED="1459104200238" TEXT="English bare recipient theme passives"/>
+</node>
+<node CREATED="1459104135060" ID="ID_864539448" MODIFIED="1459104265317" TEXT="Defective intervention/Dative raising dispreference">
+<node CREATED="1459104209545" ID="ID_288037598" MODIFIED="1459104220756" TEXT="Swedish monotransitives"/>
+<node CREATED="1459104221252" ID="ID_200027473" MODIFIED="1459104228538" TEXT="Early modern English?"/>
+<node CREATED="1459104230382" ID="ID_1140325567" MODIFIED="1459104234925" TEXT="Faroese"/>
+</node>
+<node CREATED="1459104141918" ID="ID_946663513" MODIFIED="1459104147803" TEXT="Accessible datives">
+<node CREATED="1459104271671" ID="ID_1389468819" MODIFIED="1459104349424" TEXT="Normal recipient passives">
+<node CREATED="1459104285491" ID="ID_460125488" MODIFIED="1459104290312" TEXT="Ancient Greek"/>
+<node CREATED="1459104290901" ID="ID_225423989" MODIFIED="1459104312701" TEXT="English, Norwegian and Danish recipient passives"/>
+</node>
+<node CREATED="1459104349921" ID="ID_786263420" MODIFIED="1459104356100" TEXT="Specially marked recipient passives">
+<node CREATED="1459104313275" ID="ID_901808835" MODIFIED="1459104322392" TEXT="German and Dutch kriegen/bekommen passives"/>
+<node CREATED="1459104322807" ID="ID_66336923" MODIFIED="1459104331126" TEXT="Icelandic -st middles?"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1459116554333" ID="ID_1541166546" MODIFIED="1459116561419" POSITION="right" TEXT="Formalisation">
+<node CREATED="1459116978296" ID="ID_864801711" MODIFIED="1459116989763" TEXT="Active ditransitive syntax">
+<node CREATED="1459117092336" ID="ID_1812286922" MODIFIED="1459117116097" TEXT="Adopt applicative structure (appl head assigns dative case)"/>
+<node CREATED="1459117103119" ID="ID_869219703" MODIFIED="1459117145602" TEXT="Scrambling moves theme to higher specifier of ApplP (a la McGinnis)"/>
+<node CREATED="1459117146017" ID="ID_910664821" MODIFIED="1459117158493" TEXT="Interaction with particle shift"/>
+</node>
+<node CREATED="1459116990269" ID="ID_1731728377" MODIFIED="1459117185015" TEXT="Active ditransitive morphology (English dative shift VIs)"/>
+<node CREATED="1459117000377" ID="ID_421691061" MODIFIED="1459117005447" TEXT="Passive ditransitive syntax">
+<node CREATED="1459117009258" ID="ID_1067152432" MODIFIED="1459117045201" TEXT="Nominative subject requirement on T"/>
+<node CREATED="1459117045625" ID="ID_1724019188" MODIFIED="1459117060989" TEXT="Dative-to-nominative: structural dative"/>
+<node CREATED="1459117061481" ID="ID_1567513302" MODIFIED="1459117077212" TEXT="Dative-to-nominative: Dative sensitive T"/>
+<node CREATED="1459117077660" ID="ID_132070634" MODIFIED="1459117086496" TEXT="Conclusions about Dative-to-nominative"/>
+</node>
+<node CREATED="1459117388204" ID="ID_433656352" MODIFIED="1459117394904" TEXT="Usage probabilities"/>
+</node>
+<node CREATED="1459116561999" ID="ID_1509436217" MODIFIED="1459116571628" POSITION="right" TEXT="Historical Implications">
+<node CREATED="1459117191851" ID="ID_140954440" MODIFIED="1459117201833" TEXT="The rise and fall of `to&apos;">
+<node CREATED="1459117308705" ID="ID_1743537745" MODIFIED="1459117328678" TEXT="Spread of `to&apos; from goal/addressee to recipient contexts"/>
+<node CREATED="1459117329287" ID="ID_278084872" MODIFIED="1459117348772" TEXT="Initially replaced dative case across the board"/>
+<node CREATED="1459117349111" ID="ID_937688177" MODIFIED="1459117368501" TEXT="Initial contextual difference in use reanalysed as grammatical"/>
+</node>
+<node CREATED="1459117202271" ID="ID_1996262218" MODIFIED="1459117216253" TEXT="Recipient passives in the history of English">
+<node CREATED="1459117218769" ID="ID_206168492" MODIFIED="1459117254057" TEXT="Recipient passives rise as grammar moves between inactive, defective intervention and accessible"/>
+<node CREATED="1459117283147" ID="ID_162190612" MODIFIED="1459117303161" TEXT="Loss of bare recipient theme--recipient actives takes away evidence for bare recipient morphology"/>
+<node CREATED="1459117254668" ID="ID_1536771998" MODIFIED="1459117277623" TEXT="Once recipient passives are extremely common, insufficient evidence for bare recipient theme passives"/>
+</node>
+</node>
+<node CREATED="1459116572125" ID="ID_415719949" MODIFIED="1459116595795" POSITION="right" TEXT="Future Implications and Conclusions">
+<node CREATED="1459116613270" ID="ID_737874858" MODIFIED="1459116616480" TEXT="Conclusions">
+<node CREATED="1459116619334" ID="ID_638109772" MODIFIED="1459116630510" TEXT="Base generation and scrambling"/>
+<node CREATED="1459116630983" ID="ID_525770166" MODIFIED="1459116639296" TEXT="Preposition and case unification"/>
+<node CREATED="1459116639696" ID="ID_248412847" MODIFIED="1459116654799" TEXT="Locality, oblique subjects and nominative objects"/>
+<node CREATED="1459116655335" ID="ID_97599808" MODIFIED="1459116661774" TEXT="Dative to nominative raising"/>
+</node>
+<node CREATED="1459116598320" ID="ID_1576277440" MODIFIED="1459116603379" TEXT="Ergativity">
+<node CREATED="1459116606852" ID="ID_1365735653" MODIFIED="1459116675835" TEXT="Syntactic vs morphological ergativity"/>
+<node CREATED="1459116676163" ID="ID_739393403" MODIFIED="1459116694934" TEXT="Absolutive as nominative"/>
+</node>
+</node>
+</node>
+</map>
