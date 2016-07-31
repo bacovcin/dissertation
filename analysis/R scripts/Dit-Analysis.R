cd4d2c054da03b3dea640f832862430ce3cd6dfa
Hezekiah Akiva Bacovcin <hbacovci@gmail.com>
2016-07-31 14:35:31 -0400

    Chapter 5 update

--- analysis/R scripts/Dit-Analysis.R
+++ analysis/R scripts/Dit-Analysis.R
@@ -268,6 +268,12 @@ old.brit.act <- brit.act
 pdf(file='../../images/brit-tp.pdf',paper='USr')
 gdat<-subset(old.brit.act,DO=='Theme Pronoun'&isDatAcc==0)
 
+mean(gdat$isTo[gdat$IO=='Recipient Pronoun'&gdat$year>1425&gdat$year<1700])
+# [1] 0.4247788
+
+mean(gdat$isTo[gdat$IO!='Recipient Pronoun'&gdat$year>1425&gdat$year<1700])
+# [1] 0.9272727
+
 gpoints<-group_by(gdat,era,IO)%>%summarise(isTo=mean(isTo),n=n())
 ggplot(gpoints,aes(era,isTo,linetype=factor(IO)))+geom_point(aes(size=log(n),pch=IO))+stat_smooth(method='loess',data=gdat,aes(x=year))+
 	scale_x_continuous(name='Year of Composition',breaks=seq(900,1900,100),labels=seq(900,1900,100))+
@@ -425,17 +431,14 @@ ggplot(bpoints,aes(era,isTo,linetype=factor(IO),colour=factor(Order)))+geom_poin
 dev.off()
 
 pdf(file='../../images/to-use-bf-1400.pdf',paper='USr')
-nbp<-subset(bpoints,Order=='Theme-recipient')
-np<-subset(pred,Order=='Theme-recipient')
-
-ggplot(nbp,aes(era,isTo,linetype=factor(IO),colour=factor(Order)))+geom_point(aes(size=log(n),pch=IO))+geom_line(data=np,aes(x=year))+
+ggplot(bpoints,aes(era,isTo,linetype=factor(IO),colour=factor(Order)))+geom_point(aes(size=log(n),pch=IO))+geom_line(data=pred,aes(x=year))+
 	scale_x_continuous(name='Year of Composition',breaks=seq(900,1900,100),labels=seq(900,1900,100))+
 	scale_y_continuous(name="% `To'-marking",breaks=c(0,.2,.4,.5,.6,.8,1),labels=c('0%','20%','40%','50%','60%','80%','100%'))+
 	scale_size_continuous(name="Log(Number of Tokens/50yrs)")+
 	scale_colour_discrete(name="Word Order")+
 	scale_linetype_discrete(name="Recipient Status")+
 	scale_shape_discrete(name="Recipient Status")+
-	coord_cartesian(xlim=c(min(np$year),1400))
+	coord_cartesian(xlim=c(min(brit.act$year),1400))
 dev.off()
 
 
